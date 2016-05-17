be7ff0275874773cfb304166d24754d8ca424a2f
Mia Gaulin <gaulin.mia@gmail.com>
2016-05-16 20:58:43 -0400

    updates to task 2

--- challenge.js
+++ challenge.js
@@ -18,22 +18,35 @@ vacation.push($(dateIn).val());
 vacation.push($(dateOut).val());
 
 // Task 2
+// still working on getting the XX of 387 to change as the page loads more items
+var city = document.getElementsByClassName("destination");
+var destination = city[1]
+
+// $(window).scroll(function() {
+//   if ($(window).data('ajaxready') == false) return;
+//   if ($(window).scrollTop() >= ($(document).height() - $(window).height()))
 var hotels = document.getElementById("hotel_listings");
 var hotelItems = new Array;
+
 for (var i = 0; i < hotels.childNodes.length; i++) {
   if (hotels.childNodes[i].nodeName == "LI") {
     hotelItems.push(hotels.childNodes[i]);
   }
 }
+// });
 
 var listing = document.getElementById("hotel_listing_container");
 
-listing.querySelector("h3").innerHTML = "Showing " + hotelItems.length + " out of 387 San Francisco Hotels";
+// next step is to avoid hardcoding the 387
+
+listing.querySelector("h3").innerHTML = "Showing " + hotelItems.length + " out of 387 hotels in " + $(destination).val();
 
 $(function(){
   var $listing = $('.listing_summary').clone();
   $('.bottom').append($listing);
 });
+
+// this was my first method, but i like the clone better:
 
 // window.addEventListener('scroll', function () {
 //   $('.bottom').append($('.listing_summary'));
